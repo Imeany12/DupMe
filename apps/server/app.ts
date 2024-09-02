ec35c9bb8414fdf3cb2931e6ff1ac3c975a22bbf
Imeany12 <oat.ueaangkun.inyaem@gmail.com>
2024-09-02 13:43:18 +0700

    feat: init prettier

--- apps/server/app.ts
+++ apps/server/app.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Dupme server is up!" });
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'Dupme server is up!' });
 });
 
 app.listen(port, () => {
