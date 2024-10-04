c83df0625ee9cea99757ce08cb377712f573815f
oatkup1a <voraphol12345@gmail.com>
2024-10-05 02:37:45 +0700

    feat: move all routes to routes folder

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { createServer } from 'http';
 import mongoose from 'mongoose';
 import { Server } from 'socket.io';
 
 import { CORS_ORIGIN, MONGO_URL, PORT } from './env';
+import loginRoutes from './routes/login';
+import mainRoutes from './routes/main';
 import userRoutes from './routes/user';
 
 const app = express();
 app.use(express.json());
 
 app.use('/user', userRoutes);
+app.use('/', mainRoutes);
+app.use('/login', loginRoutes);
 
 const server = createServer(app);
 const io = new Server(server, {
@@ -23,14 +27,6 @@ mongoose
   .then(() => console.log('Database connection established'))
 
   .catch((error) => console.error('Error connecting to database:', error));
-
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'DupMe server is up!' });
-});
-
-app.get('/login', (req: Request, res: Response) => {
-  res.json({ message: 'pedo time and saygex' });
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
