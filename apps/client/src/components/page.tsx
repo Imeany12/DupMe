133f67ed637d4fecb4c94c0add2397f3cf7625ad
vikimark <mark.phurit@gmail.com>
2024-10-06 02:04:14 +0700

    feat: chat feature

---
+++ apps/client/src/components/page.tsx
@@ -0,0 +1,90 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+
+import style from './chat.module.css';
+
+interface IMsgDataTypes {
+  roomId: string | number;
+  user: string;
+  msg: string;
+  time: string;
+}
+
+const ChatPage = ({ socket, username, roomId }: any) => {
+  const [currentMsg, setCurrentMsg] = useState('');
+  const [chat, setChat] = useState<IMsgDataTypes[]>([]);
+
+  const sendData = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (currentMsg !== '') {
+      const msgData: IMsgDataTypes = {
+        roomId: roomId,
+        user: username,
+        msg: currentMsg,
+        time:
+          new Date(Date.now()).getHours() +
+          ':' +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit('send_msg', msgData);
+      setChat((pre) => [...pre, msgData]);
+      setCurrentMsg('');
+    }
+  };
+
+  useEffect(() => {
+    socket.on('receive_msg', (data: IMsgDataTypes) => {
+      setChat((pre) => [...pre, data]);
+    });
+
+    return () => socket.off('receive_msg'); // Don't forget to clean up!
+  }, [socket]);
+
+  return (
+    <div className={style.chat_div}>
+      <div className={style.chat_border}>
+        <div style={{ marginBottom: '1rem' }}>
+          <p>
+            Name: <b>{username}</b> and Room Id: <b>{roomId}</b>
+          </p>
+        </div>
+        <div>
+          {chat.map(({ roomId, user, msg, time }, key) => (
+            <div
+              key={key}
+              className={
+                user == username
+                  ? style.chatProfileRight
+                  : style.chatProfileLeft
+              }
+            >
+              <span
+                className={style.chatProfileSpan}
+                style={{ textAlign: user == username ? 'right' : 'left' }}
+              >
+                {user.charAt(0)}
+              </span>
+              <h3 style={{ textAlign: user == username ? 'right' : 'left' }}>
+                {msg}
+              </h3>
+            </div>
+          ))}
+        </div>
+        <div>
+          <form onSubmit={(e) => sendData(e)}>
+            <input
+              className={style.chat_input}
+              type='text'
+              value={currentMsg}
+              placeholder='Type your message..'
+              onChange={(e) => setCurrentMsg(e.target.value)}
+            />
+            <button className={style.chat_button}>Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -19,6 +19,9 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: CORS_ORIGIN,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
   },
 });
 
@@ -30,6 +33,21 @@ mongoose
 
 io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
+  socket.on('join_room', (roomId) => {
+    socket.join(roomId);
+    console.log(`user with id-${socket.id} joined room-${roomId}`);
+  });
+
+  socket.on('send_msg', (data) => {
+    console.log(data, 'DATA');
+
+    // This will send a message to a specific room ID
+    socket.to(data.roomId).emit('receive_msg', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected:', socket.id);
+  });
 });
 
 server.listen(PORT, () => {
