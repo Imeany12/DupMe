9f81fdf9e163246f003333ff89b41e1a09539196
vikimark <mark.phurit@gmail.com>
2024-10-06 02:04:14 +0700

    feat: chat feature

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -9,6 +9,9 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: CORS_ORIGIN,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
   },
 });
 
@@ -22,6 +25,21 @@ app.get('/login', (req: Request, res: Response) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected:', socket.id);
+  socket.on('join_room', (roomId) => {
+    socket.join(roomId);
+    console.log(`user with id-${socket.id} joined room-${roomId}`);
+  });
+
+  socket.on('send_msg', (data) => {
+    console.log(data, 'DATA');
+
+    // This will send a message to a specific room ID
+    socket.to(data.roomId).emit('receive_msg', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('a user disconnected:', socket.id);
+  });
 });
 
 server.listen(PORT, () => {
