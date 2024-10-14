b60f6813a79384ad34f7f284f60660085c9d9de4
Suntoh <weerawit.j@gmail.com>
2024-10-14 14:41:13 +0700

    fix: tailwind to make it ok with the background

--- apps/client/src/app/lobby/page.tsx
+++ apps/client/src/app/lobby/page.tsx
@@ -40,54 +40,56 @@ export default function LobbyPage({ username, roomId }: any) {
   };
 
   return (
-    <div className='mt-2 flex w-screen flex-col items-center px-3 py-2'>
-      <div className='mt-2 flex flex-col items-center gap-2 px-3 py-2 text-2xl'>
-        <div className='flex items-center gap-8 px-4'>
-          <h1>Lobby</h1>
-          <p>
-            Room ID: <b>{roomId}</b>
-          </p>
-        </div>
-        {session ? (
-          <div className='mx-6 flex items-center justify-center gap-3 rounded-lg border-2 border-slate-400 px-64'>
-            <Image
-              className='mx-auto mb-2 mt-2 flex rounded-full border-2 border-black shadow-black drop-shadow-xl dark:border-slate-500'
-              src={session?.user?.image ?? '/images/default-profile.png'}
-              width={50}
-              height={50}
-              alt={session?.user?.name ?? 'Profile Pic'}
-              priority={true}
-            />
-            <p>{session?.user?.name}</p>
-          </div>
-        ) : (
-          <p>guest</p>
-        )}
-        <p className='mb-8'>Waiting for players...</p>
-        <ul>
-          {players.map((player, index) => (
-            <li key={index}>{player}</li>
-          ))}
-        </ul>
-        <div className='flex w-screen items-center gap-4 px-6'>
-          <button className='w-full rounded-md border-2 px-2'>
-            Leave Game
-          </button>
-          {players.length >= 2 ? (
-            <button
-              className='flex w-full items-center rounded-md border-2 px-2 text-xl md:justify-center'
-              onClick={startGame}
-            >
-              Start Game
-            </button>
-          ) : (
-            <p className='flex w-full flex-col items-center rounded-md border-2 px-2 text-lg md:justify-center'>
-              Waiting for other players to join...
+    <div className='flex justify-center'>
+      <div className='mx-12 mt-6 flex max-w-[990px] flex-col items-center rounded-lg border-2 border-gray-600 bg-white pb-80'>
+        <div className='mt-2 flex w-full flex-col items-center gap-2 py-2 text-2xl'>
+          <div className='flex w-full items-center justify-between gap-20 px-20 pb-10 pt-4 text-3xl font-bold'>
+            <h1 className='w-full'>Lobby </h1>
+            <p className='w-full'>
+              Room ID: <b>{roomId}</b>
             </p>
+          </div>
+          {session ? (
+            <div className='mx-6 flex items-center justify-center gap-3 rounded-lg border-2 border-slate-400 px-64'>
+              <Image
+                className='mx-auto mb-2 mt-2 flex rounded-full border-2 border-black shadow-black drop-shadow-xl dark:border-slate-500'
+                src={session?.user?.image ?? '/images/default-profile.png'}
+                width={50}
+                height={50}
+                alt={session?.user?.name ?? 'Profile Pic'}
+                priority={true}
+              />
+              <p>{session?.user?.name}</p>
+            </div>
+          ) : (
+            <p>guest</p>
           )}
-          {isGameStarting && <p>Game is starting...</p>}
+          <p className='mb-8'>Waiting for players...</p>
+          <ul>
+            {players.map((player, index) => (
+              <li key={index}>{player}</li>
+            ))}
+          </ul>
+          <div className='flex w-full items-center justify-between gap-4 px-6'>
+            <button className='w-full rounded-md border-2 px-2'>
+              Leave Game
+            </button>
+            {players.length >= 2 ? (
+              <button
+                className='flex w-full items-center rounded-md border-2 px-2 text-xl md:justify-center'
+                onClick={startGame}
+              >
+                Start Game
+              </button>
+            ) : (
+              <p className='flex w-full flex-col items-center rounded-md border-2 px-2 text-lg md:justify-center'>
+                Waiting for other players to join...
+              </p>
+            )}
+            {isGameStarting && <p>Game is starting...</p>}
+          </div>
+          {/* {chatroom} */}
         </div>
-        {/* {chatroom} */}
       </div>
     </div>
   );
