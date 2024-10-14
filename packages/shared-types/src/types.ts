bad25e7bb8a663a28324b3b8180c6fb1ba952fb8
Voraphol Lertchaiudomchok <oatkup1a@Voraphols-MacBook-Air.local>
2024-10-14 17:04:53 +0700

    feat: user addScore, update, reset, addSAM, removeUser

--- packages/shared-types/src/types.ts
+++ packages/shared-types/src/types.ts
@@ -6,7 +6,18 @@ export interface IUser {
   createdAt: Date;
   games_won: number;
   games_lost: number;
+    games_draw: number;
+    total_score: number;
+    matchHistory: IMatch[]; 
 }
+
+export interface IMatch {
+    opponent: string;
+    outcome: 'win' | 'lose' | 'draw';
+    roundsWon: number; 
+    roundsLost: number;
+    dateTime: Date;
+  }
 
 export interface IMsgDataTypes {
   roomId: string | number;
