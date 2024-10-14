c43d91e6103e3f7c4da4f7c55c0987e7dfa8329c
Suntoh <weerawit.j@gmail.com>
2024-10-14 15:17:17 +0700

    feat: handleInput

---
+++ packages/shared-types/src/types.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+    username?: string ;
+    email?: string ;
+    image?: string ;
+    password?: string; 
+    createdAt?: Date ;
+    dob?: Date ;
+    gender?: string;
+    bio?: string;
+    games_won?: number; 
+    games_lost?: number;
+} 
