4659a8d3f50794135d969752b7a014fc29e7faa6
Suntoh <weerawit.j@gmail.com>
2024-10-14 15:17:17 +0700

    feat: handleInput

--- apps/client/types.d.ts
+++ apps/client/types.d.ts
@@ -5,3 +5,15 @@ type User =
       image?: string | null | undefined;
     }
   | undefined;
+
+type IUser =
+  | {
+      username?: string | null | undefined;
+      email?: string;
+      image?: string | null | undefined;
+      password?: string | null | undefined;
+      createdAt?: Date | null | undefined;
+      games_won?: number | null | undefined;
+      games_lost?: number | null | undefined;
+    }
+  | undefined;
