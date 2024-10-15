424e3e8c0821559d0dbb43e8767518338f03bc0b
Suntoh <weerawit.j@gmail.com>
2024-10-15 18:35:01 +0700

    chore: change filename

--- packages/shared-types/src/types.ts
+++ packages/shared-types/src/types.ts
@@ -1,9 +1,15 @@
+interface KeyMapping {
+  [key: string]: string;
+}
+
+
 export interface IUser {
-  username: string;
-  password: string;
-  email?: string;
-  image?: string;
-  createdAt: Date;
-  games_won: number;
-  games_lost: number;
+  username?: string 
+  email?: string 
+  image?: string 
+  password?: string 
+  createdAt?: Date 
+  games_won?: number 
+  games_lost?: number
+  keybindings?: KeyMapping
 }
