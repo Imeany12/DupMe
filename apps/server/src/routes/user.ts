cfe9bdeb823a1059c024f819138f70187d4bbd4f
oatkup1a <voraphol12345@gmail.com>
2024-10-15 10:31:36 +0700

    feat: add routes for removeUser

--- apps/server/src/routes/user.ts
+++ apps/server/src/routes/user.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
-import { createUser, getUsers, loginUser } from '../controllers/user';
+import {
+  createUser,
+  getUsers,
+  loginUser,
+  removeUser,
+} from '../controllers/user';
 
 const router: Router = Router();
 
 router.get('/list', getUsers);
 router.post('/signup', createUser);
 router.post('/login', loginUser);
+router.post('/deleteAccount', removeUser);
 
 export default router;
