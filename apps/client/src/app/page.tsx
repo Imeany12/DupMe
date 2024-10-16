7d4c401477e108850c6934c01b1211b6638b78cf
Suntoh <weerawit.j@gmail.com>
2024-10-16 09:47:52 +0700

    feat: get room id

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -8,10 +8,16 @@ import NavBar from '@/components/NavBar';
 import UserProfile from '@/components/UserProfile';
 
 export default function Home() {
+  const [roomId, setRoomId] = useState(0);
   const [mode, setMode] = useState(false);
   const { data: session } = useSession({
     required: false,
   });
+
+  const setUpGame = () => {
+    setRoomId(Math.floor(Math.random() * 1000000));
+    setMode(true);
+  };
 
   return (
     <div>
@@ -34,19 +40,19 @@ export default function Home() {
             <div>
               <button
                 className='mt-32 rounded-lg border border-gray-500 bg-gray-200 px-8 py-2 text-4xl text-sky-600'
-                onClick={() => setMode(true)}
+                onClick={setUpGame}
               >
                 Play
               </button>
             </div>
           ) : (
             <div>
-              <button
+              <Link
                 className='mt-32 rounded-lg border border-gray-500 bg-gray-200 px-8 py-2 text-4xl text-sky-600'
-                onClick={() => setMode(false)}
+                href={`/lobby/${roomId}`}
               >
                 2 Player
-              </button>
+              </Link>
             </div>
           )}
         </div>
