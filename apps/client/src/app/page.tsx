9d825912f792c06d8eaf76efed77bdd88c1976d1
Suntoh <weerawit.j@gmail.com>
2024-10-18 18:39:40 +0700

    feat: konami

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -3,9 +3,13 @@
 import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 
+import Snowfall from '@/components/Snowfall';
 import NavBar from '@/components/NavBar';
+import Konami from '@/lib/Konami';
+import UserProfile from '@/components/UserProfile';
 
 export default function Home() {
+  const [snowing, setSnowing] = useState(false);
   const [mode, setMode] = useState(false);
   const { data: session } = useSession({
     required: false,
@@ -15,29 +19,32 @@ export default function Home() {
     <div>
       <NavBar session={session} />
       <div className='mx-auto flex h-screen w-screen flex-col items-center'>
+      <Konami triggerSnow={() => setSnowing(true)} />
+      {snowing && <Snowfall />}
         <div className='text-center text-[180px] font-semibold text-white'>
           DupMe
         </div>
-        {!mode ? (
-          <div className='flex h-screen flex-row items-center'>
-            <button
-              className='shadow-[0_4px_4px_0px_rgba(0, 0, 0, 0.25)] w-500px my-4 mb-36 rounded-full border border-solid border-black bg-[#5B687C] px-32 py-2 text-4xl text-[#FFFFFF] shadow-lg hover:bg-[#8572b5]'
-              onClick={() => setMode(true)}
-            >
-              Play
-            </button>
-          </div>
-        ) : (
-          <div>
-            <button
-              className='shadow-[0_4px_4px_0px_rgba(0, 0, 0, 0.25)] w-500px my-64 rounded-full border border-solid border-black bg-[#5B687C] px-8 py-2 text-4xl text-[#FFFFFF] shadow-lg'
-              onClick={() => setMode(false)}
-            >
-              2 Player
-            </button>
-          </div>
-        )}
-      </div>
+
+      {!mode ? (
+        <div className='flex h-screen flex-row items-end'>
+          <button
+            className='my-64 rounded-lg border border-gray-500 bg-gray-200 px-8 py-2 text-4xl text-sky-600'
+            onClick={() => setMode(true)}
+          >
+            Play
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button
+            className='my-64 rounded-lg border border-gray-500 bg-gray-200 px-8 py-2 text-4xl text-sky-600'
+            onClick={() => setMode(false)}
+          >
+            2 Player
+          </button>
+        </div>
+      )}
+    </div>
     </div>
   );
 }
