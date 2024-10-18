7728ed6413ab26b574475ffd197de7a0d1624067
Suntoh <weerawit.j@gmail.com>
2024-10-18 18:39:40 +0700

    feat: konami

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -4,10 +4,14 @@ import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 
+import Snowfall from '@/components/Snowfall';
 import NavBar from '@/components/NavBar';
+import Konami from '@/lib/Konami';
+import UserProfile from '@/components/UserProfile';
 
 export default function Home() {
   const [roomId, setRoomId] = useState(0);
+  const [snowing, setSnowing] = useState(false);
   const [mode, setMode] = useState(false);
   const { data: session } = useSession({
     required: false,
@@ -21,7 +25,9 @@ export default function Home() {
   return (
     <div>
       <NavBar session={session} />
-      <div className='mx-auto flex w-screen flex-col items-center'>
+      <div className='mx-auto flex h-screen w-screen flex-col items-center'>
+      <Konami triggerSnow={() => setSnowing(true)} />
+      {snowing && <Snowfall />}
         <div className='text-center text-[180px] font-semibold text-white'>
           DupMe
         </div>
@@ -31,7 +37,7 @@ export default function Home() {
             <div className='my-auto flex flex-row items-center'>
               <button
                 className='shadow-[0_4px_4px_0px_rgba(0, 0, 0, 0.25)] rounded-full border border-solid border-black bg-[#5B687C] px-32 py-2 text-4xl text-[#FFFFFF] shadow-lg hover:bg-[#8572b5]'
-                onClick={() => setUpGame()}
+                onClick={() => {setUpGame();setSnowing(false)}}
               >
                 Play
               </button>
