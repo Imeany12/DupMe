83615fd277528f12786a03d9cac5b864c4dc634c
Suntoh <weerawit.j@gmail.com>
2024-10-18 09:23:44 +0700

    feat: submit register

--- apps/client/src/app/auth/signUp/page.tsx
+++ apps/client/src/app/auth/signUp/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { IUser } from '@repo/shared-types';
+import { redirect } from 'next/navigation';
 import React from 'react';
 import { useState } from 'react';
 
@@ -22,12 +23,40 @@ export default function SignUpPage() {
     });
     console.log(userInfo);
   };
+  const handleSubmit = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('http://localhost:5001/user/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userInfo),
+      });
+
+      const result = await res.json();
+
+      if (res.ok) {
+        alert('User created successfully');
+        console.log('User creation result:', result);
+      } else {
+        alert('Error creating user');
+      }
+      redirect('/login');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
 
   //need div for iput images still no idea
   return (
     <div className='h-full py-2'>
       <h1 className='py-2 pb-10 text-center text-3xl text-white'>Register</h1>
-      <form className='mx-auto w-5/6 max-w-xl rounded-xl bg-white px-8 py-2'>
+      <form
+        className='mx-auto w-5/6 max-w-xl rounded-xl bg-white px-8 py-2'
+        onSubmit={handleSubmit}
+      >
         <fieldset className='flex flex-col gap-2 border px-4 py-2'>
           <legend className='mb-2 text-2xl font-semibold text-gray-500'>
             Set up your account
@@ -36,21 +65,10 @@ export default function SignUpPage() {
             <label className='text-3xl font-semibold'>Username</label>
             <input
               className='mb-3 mt-4 w-3/5 rounded border px-2 py-2 text-lg leading-tight focus:outline-indigo-300'
-              id='name'
-              name='name'
+              id='username'
+              name='username'
               type='text'
               placeholder='IGN (In-Game Name)'
-              onChange={handleInput}
-            />
-          </div>
-          <div className='flex flex-col'>
-            <label className='text-3xl font-semibold'>Email</label>
-            <input
-              className='mb-3 mt-4 w-3/5 rounded border px-2 py-2 text-lg leading-tight focus:outline-indigo-300'
-              id='email'
-              name='email'
-              type='email'
-              placeholder='example@email.com'
               onChange={handleInput}
             />
           </div>
@@ -62,6 +80,17 @@ export default function SignUpPage() {
               name='password'
               type='password'
               placeholder='your password'
+              onChange={handleInput}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <label className='text-3xl font-semibold'>Email (Optional)</label>
+            <input
+              className='mb-3 mt-4 w-3/5 rounded border px-2 py-2 text-lg leading-tight focus:outline-indigo-300'
+              id='email'
+              name='email'
+              type='email'
+              placeholder='example@email.com'
               onChange={handleInput}
             />
           </div>
@@ -95,7 +124,7 @@ export default function SignUpPage() {
               <option value='MTF'>MTF</option>
               <option value='FTM'>FTM</option>
               <option value='Non'>Non-Binary</option>
-              <option value='Unknown'>Prefer not to say</option>
+              <option value=''>Prefer not to say</option>
             </select>
           </div>
         </fieldset>
@@ -112,6 +141,14 @@ export default function SignUpPage() {
             onChange={handleInput}
           />
         </fieldset>
+        <div className='flex w-full flex-col items-center'>
+          <button
+            type='submit'
+            className='my-4 rounded-md border-2 border-neutral-500 px-4 text-xl'
+          >
+            Register
+          </button>
+        </div>
       </form>
     </div>
   );
