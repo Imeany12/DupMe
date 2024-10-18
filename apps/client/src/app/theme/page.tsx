69f11295c6edae34e80afb29d5e028da2e10808a
Suntoh <weerawit.j@gmail.com>
2024-10-18 10:07:26 +0700

    fix:css variable

--- apps/client/src/app/theme/page.tsx
+++ apps/client/src/app/theme/page.tsx
@@ -6,7 +6,7 @@ export default function
   return (
     <div className='flex items-center flex-col py-4 gap-4 text-3xl'>
         <p className='text-white'>This is theme testing page</p>
-        <div className='text-primary bg-note2 max-w-fit'>
+        <div className='text-note0 bg-note2 rounded-lg max-w-fit'>
 
         <ToggleTheme />
         </div>
