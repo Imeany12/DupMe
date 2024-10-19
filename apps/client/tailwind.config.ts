43651bd08421ea65264149f01ee5a4eefc35f5ce
suntoh <weerawit.j@gmail.com>
2024-10-19 15:21:04 +0700

    chore: remove comment

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -15,13 +15,13 @@ const config: Config = {
       },
     },
     extend: {
-    //   backgroundImage: {
-		// 'stars': 'url("/images/starBg.png")',
-		// 'forest': 'url("/images/forestBg.png")',
-		// 'snow': 'url("/images/snowBg.png")',
-		// 'haunt': 'url("/images/hauntBg.png")',
-		// 'city': 'url("/images/cityBg.png")',
-    //   },
+      backgroundImage: {
+		'stars': 'url("/images/starBg.png")',
+		'forest': 'url("/images/forestBg.png")',
+		'snow': 'url("/images/snowBg.png")',
+		'haunt': 'url("/images/hauntBg.png")',
+		'city': 'url("/images/cityBg.png")',
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -30,73 +30,6 @@ const config: Config = {
         note1: 'var(--note1)',
         note2: 'var(--note2)',
        },
-      // themeVariants: {
-      //   'star': {
-      //     backgroundImage: 'url("/images/starBg.png")',
-      //     colors: {
-      //       //'text': '#fdd835', //yellow
-      //       primary: {
-      //         DEFAULT:'#82C5FFD9',
-      //       },note2:{
-      //         DEFAULT:'#5F89FF',
-      //       },note3:{
-      //         DEFAULT:'#0D1D2CD9',
-      //     },
-      //   },
-      //   'snow': {
-      //     backgroundImage: 'url("/images/snowBg.png")',
-      //     colors: {
-      //       //'text': '#ffffff',  // white text for snow theme
-      //       primary: {
-      //         DEFAULT:'#BDBDBDD9',
-      //       },note2:{
-      //         DEFAULT:'#EFEBF0',
-      //       },note3:{
-      //         DEFAULT:'#333322D9',
-      //     },
-      //     },
-      //   },
-      //   'city': {
-      //     backgroundImage: 'url("/images/cityBg.png")',
-      //     colors: {
-      //       //'text': '#fafafa',  // light gray text for city theme
-      //       primary: {
-      //         DEFAULT:'#EFACCD',
-      //       },note2:{
-      //         DEFAULT:'#C175D4',
-      //       },note3:{
-      //         DEFAULT:'#36083F',
-      //     },
-      //     },
-      //   },
-      //   'forest': {
-      //     backgroundImage: 'url("/images/forestBg.png")',
-      //     colors: {
-      //       //'text': '#fafafa',  // light gray text for forest theme
-      //       primary: {
-      //         DEFAULT:'#5FDAFF',
-      //       },note2:{
-      //         DEFAULT:'#8AFF82',
-      //       },note3:{
-      //         DEFAULT:'#03200F',
-      //     },
-      //     },
-      //   },
-      //   'haunt': {
-      //     backgroundImage: 'url("/images/hauntBg.png")',
-      //     colors: {
-      //       //'text': '#fafafa',  // light gray text for haunt theme
-      //       primary: {
-      //         DEFAULT:'#F2AD8C',
-      //       },note2:{
-      //         DEFAULT:'#F24545',
-      //       },note3:{
-      //         DEFAULT:'#2F1515',
-      //     },
-      //     },
-      //},
-      //},
-    //},
   },
       animation:{
         "accordion-down": "accordion-down 0.2s ease-out",
