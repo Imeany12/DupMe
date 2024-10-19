997ef29d8ca75a943d4499310ae15a11d41ba899
Suntoh <weerawit.j@gmail.com>
2024-10-19 11:22:24 +0700

    fix: merge conflict

--- packages/shared-types/src/types.ts
+++ packages/shared-types/src/types.ts
@@ -25,15 +25,4 @@ export interface IMsgDataTypes {
   user: string;
   msg: string;
   time: string;
-}export interface IUser {
-    username?: string ;
-    password?: string; 
-    image?: string ;
-    email?: string ;
-    createdAt?: Date ;
-    dob?: Date ;
-    gender?: string;
-    bio?: string;
-    games_won?: number; 
-    games_lost?: number;
-} 
+}
