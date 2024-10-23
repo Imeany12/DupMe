165ff15ae804a1835726365bbf400a80747019ec
Suntoh <weerawit.j@gmail.com>
2024-10-23 12:28:36 +0700

    feat: enter roomId to join, navigate through home-lobby-game, add toggle theme to setting page

--- apps/client/src/app/set/page.tsx
+++ apps/client/src/app/set/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 
+import { ToggleTheme } from '@/components/ui/toggle-theme';
+
 interface KeyMapping {
   [key: string]: string;
 }
@@ -48,34 +50,45 @@ export default function SettingsPage() {
   ];
 
   return (
-    <div className='flex flex-col items-center bg-white text-gray-500'>
-      <h1>Settings</h1>
-      <h2>Set Keys to Notes</h2>
-      <form className='flex flex-col items-center gap-3'>
-        {notes.map((note) => (
-          <div key={note} className='flex items-start justify-start gap-4'>
-            <label className='flex min-w-20 flex-row items-start justify-start'>
-              {note}:
-              <button
-                className='mx-3 max-w-8 items-center rounded-md border border-gray-300 p-1'
-                onClick={(event) => {
-                  event.preventDefault();
-                  const handleKeyPress = (e: KeyboardEvent) => {
-                    setKeyMappings((prev) => ({
-                      ...prev,
-                      [note]: e.key,
-                    }));
-                    window.removeEventListener('keydown', handleKeyPress);
-                  };
-                  window.addEventListener('keydown', handleKeyPress);
-                }}
-              >
-                {keyMappings[note] || 'Set Key'}
-              </button>
-            </label>
-          </div>
-        ))}
-      </form>
+    <div className='w-svh mx-10 my-12 flex flex-col items-center gap-4 rounded-lg bg-neutral-50'>
+      <div className='text-note bg-note2 mt-6 rounded-lg'>
+        <ToggleTheme />
+      </div>
+
+      <div className='flex flex-col items-center py-4 text-gray-500'>
+        <h1>Settings</h1>
+        <h2>Set Keys to Notes</h2>
+        <form className='flex flex-col items-center gap-3 rounded-lg bg-white p-4 shadow-md'>
+          {notes.map((note) => (
+            <div
+              key={note}
+              className='flex w-full items-center justify-between gap-4'
+            >
+              <label className='flex w-full flex-row items-center gap-10 text-gray-700'>
+                <span className='font-semibold'>{note}:</span>
+                <div className='ml-auto flex justify-end'>
+                  <button
+                    className='w-12 rounded-md bg-blue-500 py-1 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400'
+                    onClick={(event) => {
+                      event.preventDefault();
+                      const handleKeyPress = (e: KeyboardEvent) => {
+                        setKeyMappings((prev) => ({
+                          ...prev,
+                          [note]: e.key,
+                        }));
+                        window.removeEventListener('keydown', handleKeyPress);
+                      };
+                      window.addEventListener('keydown', handleKeyPress);
+                    }}
+                  >
+                    {keyMappings[note] || 'Set Key'}
+                  </button>
+                </div>
+              </label>
+            </div>
+          ))}
+        </form>
+      </div>
     </div>
   );
 }
