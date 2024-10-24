5008f805067be514c764faf2fc62626faea601d0
suntoh <weerawit.j@gmail.com>
2024-10-24 17:22:12 +0700

    feat: display note pressed for 2 players, decoration, prototype-playTurn

--- apps/client/src/app/game/[roomId]/page.tsx
+++ apps/client/src/app/game/[roomId]/page.tsx
@@ -35,10 +35,11 @@ export default function GamePage() {
 
   const user = session?.user ?? ({ name: 'Guest' } as User);
 
+  const [playAlong, setPlayAlong] = useState<boolean>(false);
   //const [isPlayerTurn, setIsPlayerTurn] = useState<boolean>(host);
+  //playerTurn form randaomization backend
   const [isPlayerTurn, setIsPlayerTurn] = useState<boolean>(true);
   const { roomId } = useParams<{ roomId: string }>();
-  const [play, setPlay] = useState<boolean>(false);
   const [notes, setNotes] = useState<Note[]>([]);
   const [presNote, setPresNote] = useState<pressNote>({
     pressing: false,
@@ -71,20 +72,20 @@ export default function GamePage() {
   const sendNoteToPlayer = (notes: Note[]) => {
     socket.emit('sendNote', roomId, notes);
   };
-  useEffect(() => {
-    if (!user || !socket || !roomId) return;
-    if (!hasJoined.current) {
-      socket.emit('join_lobby', { username: user.name, roomId });
-      console.log(`user ${user?.name} joined room-${roomId}`);
-      hasJoined.current = true; // Mark as joined
-    }
+  // useEffect(() => {
+  //   if (!user || !socket || !roomId) return;
+  //   if (!hasJoined.current) {
+  //     socket.emit('join_lobby', { username: user.name, roomId });
+  //     console.log(`user ${user?.name} joined room-${roomId}`);
+  //     hasJoined.current = true; // Mark as joined
+  //   }
 
-    return () => {
-      //socket.emit('leave_lobby', { roomId });
-      socket.off('update_players');
-      socket.off('start_game');
-    };
-  }, [user, socket, roomId]);
+  //   return () => {
+  //     //socket.emit('leave_lobby', { roomId });
+  //     socket.off('update_players');
+  //     socket.off('start_game');
+  //   };
+  // }, [user, socket, roomId]);
 
   useEffect(() => {
     if (!isPlayerTurn && pressedNotes.length > 0) {
@@ -94,9 +95,11 @@ export default function GamePage() {
 
   useEffect(() => {
     const handleRecieve = (recievedNote: string) => {
-      console.log('listening');
-      console.log('recieved note:', recievedNote);
-      setPressedNotes((prev) => [...prev, recievedNote.toString()]);
+      if (!isPlayerTurn && recievedNote) {
+        console.log('listening');
+        console.log('recieved note:', recievedNote);
+        setPressedNotes((prev) => [...prev, recievedNote.toString()]);
+      }
     };
     socket.on('playNote', handleRecieve);
 
@@ -182,7 +185,7 @@ export default function GamePage() {
     const note = Object.keys(keyMappings).find(
       (note) => keyMappings[note] === pressedKey
     );
-    socket.emit('sendNote', roomId, note);
+    socket.emit('getNote', roomId, note);
     //play many notes at once
     if (note && !activeOscillators[note]) {
       const frequency = getNoteFrequency(note);
@@ -278,89 +281,99 @@ export default function GamePage() {
   };
 
   return (
-    <div>
-      {isPlayerTurn ? (
-        <div className='flex h-screen w-screen flex-col items-center justify-end pb-12'>
-          <div className='flex w-full items-start justify-start'>
-            <button
-              className='size-20 px-8 pt-6 text-white'
-              onClick={() => {
-                // socket.emit('game_end');
-                // console.log('player resign');
-                // router.push('/lobby/' + roomId);
-                setIsPlayerTurn(false);
-              }}
-            >
-              <FaFontAwesomeFlag
-                size={30}
-                className='shadow-inner shadow-white'
-              />
-            </button>
-          </div>
-          <div className='max-w-screen-svh mx-16 flex max-h-svh flex-col items-center gap-8 rounded-2xl bg-slate-300 px-12 pb-8'>
-            <p className='pt-6 text-3xl text-white'>Play Your notes:</p>
-            <div className='drop max-w-screen flex min-h-[220px] flex-wrap gap-4'>
-              {pressedNotes.map((note, index) => (
-                <span
-                  key={index}
-                  className='bg-gradient-radial shirk-0 gradient flex h-16 w-16 items-center justify-center rounded-full border border-[#2FBCE7B0] bg-white from-[#C4C4C400] from-10% to-[#2FBCE7B0] text-xl font-bold text-[#6A98FF] shadow-[0_0px_40px_8px_#6A98FF]'
-                >
-                  {note}
-                </span>
-              ))}
-            </div>
-            {/* {show pressednotes here} */}
-            <div className='flex h-full flex-col justify-end pt-4'>
-              <Piano
-                onNoteClick={handleNoteClick}
-                onNoteReleased={handleNoteRelease}
-              />
-            </div>
-          </div>
-        </div>
+    <div className='flex h-screen w-screen flex-col items-center'>
+      {playAlong ? (
+        <div>{/* waiting for rainfall from mark */}</div>
       ) : (
         <div>
-          <div className='flex h-screen w-screen flex-col items-center justify-end pb-12'>
-            <div className='flex w-full items-start justify-start'>
-              <button
-                className='size-20 px-8 pt-6 text-white'
-                onClick={() => {
-                  socket.emit('game_end');
-                  console.log('player resign');
-                  router.push('/lobby/' + roomId);
-                }}
-              >
-                <FaFontAwesomeFlag
-                  size={30}
-                  className='shadow-inner shadow-white'
-                />
-              </button>
+          {isPlayerTurn ? (
+            <div>
+              <div className='flex w-full items-start justify-start'>
+                <button
+                  className='size-20 px-8 pt-6 text-white'
+                  onClick={() => {
+                    // socket.emit('game_end');
+                    // console.log('player resign');
+                    // router.push('/lobby/' + roomId);
+                    setIsPlayerTurn(false);
+                  }}
+                >
+                  <FaFontAwesomeFlag
+                    size={30}
+                    className='shadow-inner shadow-white'
+                  />
+                </button>
+              </div>
+              <div className='flex h-full flex-col justify-end'>
+                <div className='max-w-screen-svh mx-16 flex max-h-full flex-col items-center justify-end gap-8 rounded-2xl bg-slate-300 px-12 pb-8'>
+                  <p className='pt-6 text-3xl text-white'>Play Your notes:</p>
+                  <div className='drop max-w-screen flex min-h-[220px] flex-wrap gap-4'>
+                    {pressedNotes.map((note, index) => (
+                      <span
+                        key={index}
+                        className='bg-gradient-radial shirk-0 gradient flex h-16 w-16 items-center justify-center rounded-full border border-[#2FBCE7B0] bg-white from-[#C4C4C400] from-10% to-[#2FBCE7B0] text-xl font-bold text-[#6A98FF] shadow-[0_0px_40px_8px_#6A98FF]'
+                      >
+                        {note}
+                      </span>
+                    ))}
+                  </div>
+                  {/* {show pressednotes here} */}
+                  <div>
+                    <Piano
+                      onNoteClick={handleNoteClick}
+                      onNoteReleased={handleNoteRelease}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className='max-w-screen-svh mx-16 flex max-h-svh flex-col items-center gap-8 rounded-2xl bg-slate-300 px-12 pb-8'>
-              <p className='pt-6 text-3xl text-white'>Play Your notes:</p>
-              <div className='drop max-w-screen flex min-h-[220px] flex-wrap gap-4'>
-                {pressedNotes.map((note, index) => (
-                  <span
-                    key={index}
-                    className='bg-gradient-radial shirk-0 gradient flex h-16 w-16 items-center justify-center rounded-full border border-[#2FBCE7B0] bg-white from-[#C4C4C400] from-10% to-[#2FBCE7B0] text-xl font-bold text-[#6A98FF] shadow-[0_0px_40px_8px_#6A98FF]'
+          ) : (
+            <div>
+              <div className='flex h-screen w-screen flex-col items-center justify-end pb-12'>
+                <div className='flex w-full items-start justify-start'>
+                  <button
+                    className='size-20 px-8 pt-6 text-white'
+                    onClick={() => {
+                      socket.emit('game_end');
+                      console.log('player resign');
+                      router.push('/lobby/' + roomId);
+                    }}
                   >
-                    {note}
-                  </span>
-                ))}
-              </div>
-              {/* {show pressednotes here} */}
-              <div className='flex h-full flex-col justify-end pt-4'>
-                <Piano
-                  onNoteClick={() => {
-                    return;
-                  }}
-                  onNoteReleased={() => {
-                    return;
-                  }}
-                />
+                    <FaFontAwesomeFlag
+                      size={30}
+                      className='shadow-inner shadow-white'
+                    />
+                  </button>
+                </div>
+                <div className='max-w-screen-svh mx-16 flex max-h-svh flex-col items-center gap-8 rounded-2xl bg-slate-300 px-12 pb-8'>
+                  <p className='pt-6 text-3xl text-white'>
+                    Memorize your opponent notes:
+                  </p>
+                  <div className='drop max-w-screen flex min-h-[220px] flex-wrap gap-4'>
+                    {pressedNotes.map((note, index) => (
+                      <span
+                        key={index}
+                        className='bg-gradient-radial shirk-0 gradient flex h-16 w-16 items-center justify-center rounded-full border border-[#2FBCE7B0] bg-white from-[#C4C4C400] from-10% to-[#2FBCE7B0] text-xl font-bold text-[#6A98FF] shadow-[0_0px_40px_8px_#6A98FF]'
+                      >
+                        {note}
+                      </span>
+                    ))}
+                  </div>
+                  {/* {show pressednotes here} */}
+                  <div className='flex h-full flex-col justify-end pt-4'>
+                    <Piano
+                      onNoteClick={() => {
+                        return;
+                      }}
+                      onNoteReleased={() => {
+                        return;
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       )}
     </div>
