6fd48cccd8d5cce0d5420671738d2d014273d9db
suntoh <weerawit.j@gmail.com>
2024-10-24 17:22:12 +0700

    feat: display note pressed for 2 players, decoration, prototype-playTurn

--- apps/client/src/app/game/[roomId]/page.tsx
+++ apps/client/src/app/game/[roomId]/page.tsx
@@ -195,7 +195,7 @@ export default function GamePage({
     const note = Object.keys(keyMappings).find(
       (note) => keyMappings[note] === pressedKey
     );
-    socket.emit('sendNote', roomId, note);
+    socket.emit('getNote', roomId, note);
     //play many notes at once
     if (note && !activeOscillators[note]) {
       const frequency = getNoteFrequency(note);
