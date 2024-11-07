ca0363f5a62834ff4689addf79a7a1407a577d36
Suntoh <weerawit.j@gmail.com>
2024-11-08 00:18:47 +0700

    fix: lobby avatar

--- apps/client/src/pages/game/page.tsx
+++ apps/client/src/pages/game/page.tsx
@@ -772,7 +772,7 @@ export default function Game({
 
   useEffect(() => {
     console.log('keymapping:', keyMappings);
-    if (turncount.current == 2) {
+    if (turncount.current > 2 * countPlayer) {
       socket.emit('end_game', roomId, scoreComboResult[0], user?.name);
       setTimeout(() => {
         router.push('/lobby/' + roomId + '?host=' + host);
@@ -799,7 +799,7 @@ export default function Game({
       setTimeout(() => {
         setPressedNotes([]);
         setPlayAlong(true);
-      }, 3000);
+      }, 30000);
     }
     if (playAlong === true) {
       setTimeout(() => {
@@ -813,7 +813,7 @@ export default function Game({
         setIsFirstNote(true);
         setNotes(defaultNotes);
         setScoreComboResult(([score, combo, state]) => [score, 0, '']);
-      }, 5000);
+      }, 15000);
       const newSong: ISong = {
         roomId: roomId,
         user: user.name ?? 'Guest',
